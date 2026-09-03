db111f2ff1dc516c7ba00e1d030918ec7cac2f5c
Jay <Jorsinkaiye@gmail.com>
2026-09-02 22:31:28 -0400

    user can create aus and push them to database now

--- app/AUs/New/page.tsx
+++ app/AUs/New/page.tsx
@@ -1,7 +1,60 @@
+"use client";
+
 import Button from "@/components/Button";
 import Input from "@/components/Input";
+import {useRouter} from "next/navigation";
+import {createClient} from "@/lib/supabase/client";
+import {useState} from "react";
 
 export default function New() {
+    const router = useRouter();
+    const supabase = createClient();
+
+    const [title, setTitle] = useState("");
+    const [description, setDescription] = useState("");
+    const [error, setError] = useState("");
+    const [loading, setLoading] = useState(false);
+
+    async function createAU(){
+        setError("");
+
+        if(!title.trim()){
+            setError("Please enter a title");
+            return;
+        }
+
+        setLoading(true);
+
+        const{
+            data: {user},
+            error: userError
+        } = await supabase.auth.getUser();
+
+        if(userError || !user){
+            setError("You must be logged in to create an AU.");
+            setLoading(false);
+            return;
+        }
+
+        const {data, error: insertError } = await supabase
+            .from("aus")
+            .insert({
+                user_id: user.id,
+                title: title,
+                description: description.trim() || null,
+            })
+            .select()
+            .single();
+
+        if(insertError){
+            setError(insertError.message);
+            setLoading(false);
+            return;
+        }
+
+        router.push(`/AUs/${data.id}`);
+    }
+
     return(
         <div className = "flex h-full flex-1 flex-col items-center justify-center p-10 gap-20">
 
@@ -20,7 +73,8 @@ export default function New() {
                     <Input type="text" placeholder="Description"/>
                 </div>
 
-                <Button text="Create AU"/>
+                <Button text={loading ? "Creating..." : "Create AU"}
+                onClick={createAU}/>
             </div>
 
         </div>
---
+++ app/AUs/[id]/page.tsx
@@ -0,0 +1,13 @@
+export default async function AUPage({
+                                         params,
+                                     }: {
+    params: Promise<{ id: string }>
+}) {
+    const { id } = await params;
+
+    return (
+        <div>
+            <h1>AU ID: {id}</h1>
+        </div>
+    );
+}
